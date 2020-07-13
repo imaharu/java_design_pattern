ec3dd75dca25a88630750a9e10b59e545342dbe3
imaharu <light1200out@gmail.com>
2020-07-14 06:50:37 +0900

    Vistorの写経

---
+++ Visitor/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		try {
+			System.out.println("Making root entries...");
+			Directory rootdir = new Directory("root");
+			Directory bindir = new Directory("bin");
+			Directory tmpdir = new Directory("tmp");
+			Directory usrdir = new Directory("usr");
+			rootdir.add(bindir);
+			rootdir.add(tmpdir);
+			rootdir.add(usrdir);
+			bindir.add(new File("vi", 10000));
+			bindir.add(new File("latex", 10000));
+			rootdir.accept(new ListVisitor());
+		} catch (FileTreatmentException e) {
+			e.printStackTrace();	
+		}
+	}
+}
